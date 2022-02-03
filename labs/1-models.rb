76c5c14d447bb380260c486525dd1fae4184c228
bill-chase-kellogg <bill.chase@kellogg.northwestern.edu>
2022-02-03 02:23:29 +0000

    finished lab

--- labs/1-models.rb
+++ labs/1-models.rb
@@ -11,6 +11,10 @@ Contact.destroy_all
 # 1a. check out the schema file
 # 1b. check out the model file
 
+apple = Company.where({name: "Apple"})[0]
+amazon = Company.where({name: "Amazon"})[0]
+tesla = Company.where({name: "Tesla"})[0]
+
 # 2. create 1-2 new contacts for each company (they can be made up)
 
 # puts "There are #{Contact.all.count} contacts"
@@ -19,27 +23,29 @@ andy = Contact.new
 andy.write_attribute(:first_name, "Andy")
 andy.write_attribute(:last_name, "Jassy")
 andy.write_attribute(:email, "andy@amazon.com")
+andy.write_attribute(:company_id, amazon.id )
 andy.save
 
 craig = Contact.new
 craig.write_attribute(:first_name, "Craig")
 craig.write_attribute(:last_name, "Federighi")
 craig.write_attribute(:email, "craig@apple.com")
+craig.write_attribute(:company_id, apple.id)
 craig.save
 
 elon = Contact.new
 elon.write_attribute(:first_name, "Elon")
 elon.write_attribute(:last_name, "Musk")
 elon.write_attribute(:email, "elon@tesla.com")
+elon.write_attribute(:company_id, tesla.id)
 elon.save
 
 tim = Contact.new
 tim.write_attribute(:first_name, "Tim")
 tim.write_attribute(:last_name, "Cook")
 tim.write_attribute(:email, "tim@apple.com")
+tim.write_attribute(:company_id, apple.id)
 tim.save
-
-
 
 # 3. write code to display how many contacts are in the database AND each contact's info (name, email), e.g.:
 
@@ -51,8 +57,10 @@ tim.save
 # Tim Cook - tim@apple.com
 
 puts "Contacts: #{Contact.all.count}"
-puts 
-"#{andy.first_name} #{andy.last_name} - #{andy.email}
-#{craig.first_name} #{craig.last_name} - #{craig.email}
-#{elon.first_name} #{elon.last_name} - #{elon.email} 
-#{tim.first_name} #{tim.last_name} - #{tim.email}"
+# puts Contact.all.inspect
+contacts = Contact.all
+
+for contact in contacts
+    puts "#{contact.first_name} #{contact.last_name} - #{contact.email}"
+end
+
