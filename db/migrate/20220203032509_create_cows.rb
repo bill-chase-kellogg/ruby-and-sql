7ed9c75e6f836bb297d5212fd75f4ecc831b8b92
bill-chase-kellogg <bill.chase@kellogg.northwestern.edu>
2022-02-03 16:12:23 +0000

    commit

---
+++ db/migrate/20220203032509_create_cows.rb
@@ -0,0 +1,10 @@
+class CreateCows < ActiveRecord::Migration[7.0]
+  def change
+    create_table :cows do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_03_030737) do
+ActiveRecord::Schema.define(version: 2022_02_03_032509) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,19 @@ ActiveRecord::Schema.define(version: 2022_02_03_030737) do
     t.string "email"
     t.string "phone_number"
     t.integer "company_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "cows", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "salespeople", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
