d5303ddc244a67e2d70d5d11e69f2aa13cbbba3a
bill-chase-kellogg <bill.chase@kellogg.northwestern.edu>
2022-02-03 02:33:17 +0000

    associations

--- code-along/2-associations.rb
+++ code-along/2-associations.rb
@@ -5,6 +5,19 @@
 
 # 1. Query all of the contacts at Apple Inc.
 
+apple = Company.where({name: "Apple"})[0]
+# amazon = Company.where({name: "Amazon"})[0]
+# tesla = Company.where({name: "Tesla"})[0]
+
+# contacts = Contact.where({company_id: apple.id})
+# puts contacts.inspect
+
+contacts = apple.contacts
+
+for contact in contacts
+    puts "#{contact.first_name} #{contact.last_name} - #{contact.email} - #{contact.company.name}"
+end
+
 # 2. add association to contacts in Company model
 
 # 3. add association to company in Contact model
